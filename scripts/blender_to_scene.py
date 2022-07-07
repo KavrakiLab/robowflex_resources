9e3b6dc615116d4c177b2a7c0d68871fa2889f6e
Zak Kingston <zak@rice.edu>
2022-07-07 16:10:32 -0500

    baby proofing

--- scripts/blender_to_scene.py
+++ scripts/blender_to_scene.py
@@ -4,11 +4,60 @@ import bpy
 from bpy.app.handlers import persistent
 import subprocess
 import sys
+import os
 
 
-def add_path(path):
-    if not path in sys.path:
-        sys.path.append(path)
+@persistent
+def load_handler(dummy):
+    # Change working directory to script location
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    # Get base name of .blend file
+    filename = bpy.path.basename(bpy.context.blend_data.filepath)
+
+    # Unselect all objects
+    for ob in bpy.context.scene.objects:
+        ob.select_set(False)
+
+    scene = {'world': {'collision_objects': []}}
+    for ob in bpy.context.scene.objects:
+        if ob.type != 'MESH':
+            continue
+
+        # Select and export mesh as COLLADA file.
+        ob.select_set(True)
+        bpy.ops.wm.collada_export(filepath=os.path.abspath(
+            f'../objects/{filename}/{ob.data.name}.dae'),
+                                  selected=True)
+        ob.select_set(False)
+
+        # Get object properties
+        scale = list(ob.scale)
+        position = list(ob.matrix_world.to_translation())
+        q = list(ob.matrix_world.to_quaternion())
+        quaternion = q[1:] + q[:1]  # Rotate quaternion to XYZW order
+
+        # Add object to scene.
+        scene['world']['collision_objects'].append({
+            'id':
+            ob.data.name,
+            'meshes': [{
+                'resource':
+                f'package://robowflex_resources/objects/{filename}/{ob.data.name}.dae',
+                'dimensions': scale
+            }],
+            'mesh_poses': [{
+                'position': position,
+                'orientation': quaternion
+            }]
+        })
+
+    output = dump(scene, Dumper=Dumper, default_flow_style=False)
+    output_filename = os.path.abspath(f'../objects/{filename}/{filename}.yaml')
+    with open(output_filename, 'w') as f:
+        f.write(output)
+
+    print(f"Wrote scene to {output_filename}")
 
 
 def initialize_path():
@@ -18,57 +67,22 @@ def initialize_path():
         paths = output.decode().strip().split('\n')
 
         for path in paths:
-            add_path(path)
+            if not path in sys.path:
+                sys.path.append(path)
 
     except subprocess.CalledProcessError:
         print("Unable to call system Python3")
         return ""
 
 
-@persistent
-def load_handler(dummy):
-    filename = bpy.path.basename(bpy.context.blend_data.filepath)
-    scene = {'world': {'collision_objects': []}}
-
-    for ob in bpy.context.scene.objects:
-        ob.select_set(False)
-
-    for ob in bpy.context.scene.objects:
-        ob.select_set(True)
-        bpy.ops.wm.collada_export(
-            filepath=f'../objects/{filename}/{ob.data.name}.dae',
-            selected=True)
-
-        q = list(ob.matrix_world.to_quaternion())
-        q = q[1:] + q[:1]  # Rotate quaternion to XYZW order
-        scene['world']['collision_objects'].append({
-            'id':
-            ob.data.name,
-            'meshes': [{
-                'resource':
-                f'package://robowflex_resources/objects/{filename}/{ob.data.name}.dae',
-                'dimensions': list(ob.scale)
-            }],
-            'mesh_poses': [{
-                'position': list(ob.matrix_world.to_translation()),
-                'orientation': q
-            }]
-        })
-
-        ob.select_set(False)
-
-    output = dump(scene, Dumper=Dumper, default_flow_style=False)
-    with open(f'../objects/{filename}/{filename}.yaml', 'w') as f:
-        f.write(output)
-
-    print(output)
-
-
+# Initialize Python path with system Python
 initialize_path()
+
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
 
+# Install handler to run after loading .blend file
 bpy.app.handlers.load_post.append(load_handler)
